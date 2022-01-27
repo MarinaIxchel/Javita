675fa6a560114e09b9a688e3732e4821675a24b8
Marina Ixchel <marina.ma.aa02@gmail.com>
2022-01-27 15:43:36 -0600

    Primeros pasos en Java

---
+++ NumeroParImpar/src/NumeroParImpar.java
@@ -0,0 +1,23 @@
+
+package ejercicio;
+import java.util.Scanner;
+
+public class Pares {
+	static Scanner entrada = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		System.out.println("Ingresar un numero: ");
+		numero = entrada.nextInt();
+		
+		if (numero % 2 == 0)
+			
+		System.out.println("Par");
+		else
+			System.out.println("Impar");
+		
+	}
+
+}
